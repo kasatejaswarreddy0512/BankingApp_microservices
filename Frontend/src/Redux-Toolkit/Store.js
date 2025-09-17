f17b3d4e1c72cb0fa1e1ff5712c4c1a078d224d8
kasatejaswarreddy0512 <ktrreddy64@gmail.com>
2025-09-17 17:30:09 +0530

    commit

--- Frontend/src/Redux-Toolkit/Store.js
+++ Frontend/src/Redux-Toolkit/Store.js
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authReducer from "./AuthSlice";
 import accountReducer from "./AccountSlice";
+import transactionReducer from "./TransactionSlice";
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  account: accountReducer
+  account: accountReducer,
+  transaction: transactionReducer
 
 });
 
