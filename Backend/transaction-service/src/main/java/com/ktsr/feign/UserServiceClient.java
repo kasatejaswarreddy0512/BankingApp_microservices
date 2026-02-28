9c9572b3c8465050e2ad7d426b7bd7654a294f58
kasatejaswarreddy0512 <tejaswarreddy0512@gmail.com>
2026-02-28 12:26:29 +0530

    update user services

--- Backend/transaction-service/src/main/java/com/ktsr/feign/UserServiceClient.java
+++ Backend/transaction-service/src/main/java/com/ktsr/feign/UserServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "user-service", url = "http://localhost:8081")
+@FeignClient(name = "USER-SERVICE", url = "http://localhost:8081")
 public interface UserServiceClient {
 
     @GetMapping("/api/user/profile")
